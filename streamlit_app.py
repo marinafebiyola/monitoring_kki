643ab6130e68f11a610672deb2244410b135bbf1
marinafebiyola <marinafebiyol@gmail.com>
2024-07-31 17:23:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-st.title("Streamlit dengan Backend Flask")
+st.title("Monitoring")
 
 if st.button("Dapatkan Data Acak"):
     response = requests.get("https://floppy-groups-mix.loca.lt/random-data")
