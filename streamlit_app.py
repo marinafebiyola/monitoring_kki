35757081ca31544f5e76314bd6f3dfd8b0c68882
marinafebiyola <marinafebiyol@gmail.com>
2024-07-31 17:18:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import requests
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Streamlit dengan Backend Flask")
+
+if st.button("Dapatkan Data Acak"):
+    response = requests.get("https://floppy-groups-mix.loca.lt/random-data")
+    data = response.json()
+    st.write(f"Data Acak: {data['data']}")
