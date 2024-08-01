3142f21696b0bb6583199f5bdead36f3f7f5f08a
marinafebiyola <marinafebiyol@gmail.com>
2024-08-01 13:57:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import requests
 st.title("Monitoring")
 
 if st.button("Dapatkan Data Acak"):
-    response = requests.get("https://floppy-groups-mix.loca.lt/random-data")
+    response = requests.get("https://orange-worms-hunt.loca.lt/random-data")
     data = response.json()
     st.write(f"Data Acak: {data['data']}")
