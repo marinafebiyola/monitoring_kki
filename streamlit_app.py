eb6e20f7b8d1b962b73182f4225999836e95f187
marinafebiyola <marinafebiyol@gmail.com>
2024-08-01 14:11:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,14 @@ import requests
 st.title("Monitoring")
 
 if st.button("Dapatkan Data Acak"):
-    response = requests.get("https://orange-worms-hunt.loca.lt/random-data")
-    data = response.json()
-    st.write(f"Data Acak: {data['data']}")
+    try:
+        # Mengirim permintaan GET ke backend Flask
+        response = requests.get("https://plenty-bobcats-travel.loca.lt/data")
+        # Memeriksa apakah respons berhasil
+        if response.status_code == 200:
+            data = response.json()
+            st.write(f"Data Acak: {data['value']}")
+        else:
+            st.write(f"Error: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        st.write(f"Permintaan gagal: {e}")
