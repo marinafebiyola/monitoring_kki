a005b334e448f607a112e6e10a896719e83e0b10
marinafebiyola <marinafebiyol@gmail.com>
2024-08-01 14:31:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,14 @@ st.title("Monitoring")
 data_placeholder = st.empty()
 
 while True:
-    response = requests.get("https://tangy-pears-cover.loca.lt/random-data")
-    data = response.json()
-    data_placeholder.write(f"Data Acak: {data['data']}")
+    try:
+        response = requests.get("https://tangy-pears-cover.loca.lt/random-data")
+        response.raise_for_status()  # Check if the request was successful
+        data = response.json()  # Attempt to parse the response as JSON
+        data_placeholder.write(f"Data Acak: {data['data']}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Request failed: {e}")
+    except requests.exceptions.JSONDecodeError:
+        st.error("Failed to parse JSON response")
     time.sleep(5)  # Adjust the sleep time as needed to control the update frequency
+
