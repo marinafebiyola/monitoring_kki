9690b66e8e350fb266b13fa24e026d976c02e40e
marinafebiyola <marinafebiyol@gmail.com>
2024-08-01 14:39:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,16 @@ data_placeholder = st.empty()
 
 while True:
     try:
-        response = requests.get("https://tangy-pears-cover.loca.lt/random-data")
-        response.raise_for_status()  # Check if the request was successful
-        data = response.json()  # Attempt to parse the response as JSON
+        response = requests.get("https://tangy-pears-cover.loca.lt/random-data", timeout=5)  # Tambahkan timeout
+        response.raise_for_status()  # Periksa jika request berhasil
+        data = response.json()  # Coba parse response sebagai JSON
         data_placeholder.write(f"Data Acak: {data['data']}")
     except requests.exceptions.RequestException as e:
-        st.error(f"Request failed: {e}")
+        st.error(f"Request gagal: {e}")
+        time.sleep(10)  # Tunggu lebih lama sebelum mencoba lagi
+        continue  # Lewati parsing JSON jika terjadi error
     except requests.exceptions.JSONDecodeError:
-        st.error("Failed to parse JSON response")
-    time.sleep(5)  # Adjust the sleep time as needed to control the update frequency
-
+        st.error("Gagal memparse response JSON")
+    except KeyError as e:
+        st.error(f"KeyError: {e} pada response JSON")
+    time.sleep(5)  # Sesuaikan waktu delay sesuai kebutuhan
