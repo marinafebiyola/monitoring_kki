dac928d5d37edaaf09c237d71220734b58b79fef
marinafebiyola <marinafebiyol@gmail.com>
2024-08-01 14:27:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import requests
+import time
 
 st.title("Monitoring")
 
-if st.button("Dapatkan Data Acak"):
-    response = requests.get("https://legal-poets-float.loca.lt/random-data")
+data_placeholder = st.empty()
+
+while True:
+    response = requests.get("https://tangy-pears-cover.loca.lt/random-data")
     data = response.json()
-    st.write(f"Data Acak: {data['data']}")
+    data_placeholder.write(f"Data Acak: {data['data']}")
+    time.sleep(5)  # Adjust the sleep time as needed to control the update frequency
