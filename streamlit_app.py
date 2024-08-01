49ed4c680af65f9d0b12463ece77fccaf7bce292
marinafebiyola <marinafebiyol@gmail.com>
2024-08-01 14:18:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,7 @@ import requests
 
 st.title("Monitoring")
 
-# URL dari local tunnel yang mengarah ke backend Flask Anda
-backend_url = "https://grumpy-foxes-beg.loca.lt/api/data"
-
 if st.button("Dapatkan Data Acak"):
-    try:
-        # Mengirim permintaan GET ke backend Flask
-        response = requests.get(backend_url)
-        # Memeriksa apakah respons berhasil
-        if response.status_code == 200:
-            data = response.json()
-            st.write(f"Data Acak: {data['value']}")
-        else:
-            st.write(f"Error: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        st.write(f"Permintaan gagal: {e}")
+    response = requests.get("https://legal-poets-float.loca.lt/random-data")
+    data = response.json()
+    st.write(f"Data Acak: {data['data']}")
